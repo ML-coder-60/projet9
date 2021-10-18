7883f4d5f4376c1fbe2dee3a26bda17f1b360d08
marc <marc@Formation.home>
2021-10-18 17:21:33 +0200

    Add authentification, reset passwor and new user

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,17 @@
+# authentication/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ADMINISTRATOR = 'ADMINISTRATOR'
+    USER = 'USER'
+
+    ROLE_CHOICES = (
+        (ADMINISTRATOR, 'Administrateur'),
+        (USER, 'Utilisateur'),
+    )
+
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
+    username = models.EmailField(unique=True)
+    EMAIL_FIELD = 'username'
--- litreview/settings.py
+++ litreview/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'litreview.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +127,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
+
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_URL = LOGIN_URL
